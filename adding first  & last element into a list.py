d24a8100a9070eb048f08ba4ba7989655deff76d
Rajat <rdeshwal32@gmail.com>
2018-07-16 12:44:22 +0530

    add first and last element into a new list

---
+++ adding first  & last element into a list.py
@@ -0,0 +1,7 @@
+my_list = [1, 2, 3, 4, 5, 6, 7]
+first_element = my_list[0]
+last_element = my_list[-1]
+new_list = []
+new_list.append(first_element)
+new_list.append(last_element)
+print('first and last:', new_list)
